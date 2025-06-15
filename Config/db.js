a95f6c209d67fad5e5cb74eb60259d86b5ccffff
bettercodevivek <viveksingh0520@gmail.com>
2025-06-15 19:59:11 +0530

    Implmented ConnectDB in Config and initialised the project structure

---
+++ Config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ConnectDB = async() => {
+    try{
+      await mongoose.connect(process.env.MONGOOSE_URL);
+        console.log('Connection with DB established successfully !')
+    }
+    catch(err){
+        console.log('Connection with DB Failed',err);
+        process.exit(1);
+    }
+}
+
+module.exports = ConnectDB;
