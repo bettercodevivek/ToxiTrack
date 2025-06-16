f46b6bac9b8ea700c64621a5eb41b5ca9ac951fe
bettercodevivek <viveksingh0520@gmail.com>
2025-06-16 17:32:44 +0530

    Implemented UserModel in Models

---
+++ Models/UserModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['admin','user','moderator'],
+        default:'user'
+    }
+},{timestamps:true});
+
+UserSchema.pre('save', async function(){
+    const user = this;
+    const saltRounds = 10;
+
+    if(!user.isModified('password')) next();
+
+     try{
+        
+        const hashedPwd = await bcrypt.hash(user.password,saltRounds);
+        
+        user.password = hashedPwd;
+
+        next();
+
+
+      }
+      catch(err){
+        next(err);
+      }
+});
+
+
+const User = mongoose.model('User',UserSchema);
+
+module.exports = User;
